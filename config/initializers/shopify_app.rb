e619bce37d15f94097b64286a62098b2250cec39
geodesfour <dgeo@DESKTOP-EL3PKCS.localdomain>
2018-02-28 22:08:15 +0100

    genrate4

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "eeeeec482eb67546ab3b00b5b624be3f1a7"
+  config.api_key = "5ec482eb67546ab3b00b5b624be3f1a7"
   config.secret = "f5abdbbb32aa7132153677838f8d7226"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
