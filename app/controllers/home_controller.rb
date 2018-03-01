7fa8f613ff53eeb0e329648b834a528434710ccb
geodesfour <dgeo@DESKTOP-EL3PKCS.localdomain>
2018-03-01 19:04:24 +0100

    test add file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @webhooks = ShopifyAPI::Webhook.find(:all)
-    @themes = ShopifyAPI::Theme.find(:all)
+    @webhooks = ShopifyAPI::Webhook.find(:all)   
   end
 end
