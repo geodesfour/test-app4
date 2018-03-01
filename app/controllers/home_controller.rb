471ea897b0c9b57ff6d64088a0a013b47106184d
geodesfour <dgeo@DESKTOP-EL3PKCS.localdomain>
2018-03-01 18:55:31 +0100

    test add file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
+    @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 5, :order => "created_at DESC" })
   end
 end
