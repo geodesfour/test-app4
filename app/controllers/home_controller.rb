d426fdb054370570437fae15e4d4db39e0cd4e1f
Geoffrey Desfour <gdesfour@gmail.com>
2018-05-07 18:37:24 +0200

    make it better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,13 @@ class HomeController < ShopifyApp::AuthenticatedController
     @shop = ShopifyAPI::Shop.current
     
   end
+  def form_page
+    if request.post?
+      if params[:name].present?
+        flash[:notice] = "Created #{ params[:colour] } unicorn: #{ params[:name] }."
+      else
+        flash[:error] = "Name must be set."
+      end
+    end
+  end
 end
