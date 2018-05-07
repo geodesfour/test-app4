446f5d9b2b1ad62dcfc311c0e9daa77788c59d75
Geoffrey Desfour <gdesfour@gmail.com>
2018-05-07 11:04:46 +0200

    app utilitaire 3er

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10000 })
+    @products = ShopifyAPI::Product.find(:all)
     @webhooks = ShopifyAPI::Webhook.find(:all)   
     @shop = ShopifyAPI::Shop.current
     
