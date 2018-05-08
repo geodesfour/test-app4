b518110948af77bdf7a377e938fc15c0821216a4
Geoffrey Desfour <gdesfour@gmail.com>
2018-05-09 00:47:32 +0200

    add faq

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all)
-    
+    @webhooks = ShopifyAPI::Webhook.find(:all)
+    @collections = ShopifyAPI::CollectionListing.find(:all)    
     @shop = ShopifyAPI::Shop.current     
   end   
 end
