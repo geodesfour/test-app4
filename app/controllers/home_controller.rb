636cddf9c8061aec0b02ae32066a9391d2dbacc2
Geoffrey Desfour <gdesfour@gmail.com>
2018-05-08 20:16:03 +0200

    add faq

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,8 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all)
-    @webhooks = ShopifyAPI::Webhook.find(:all)     
-    @shop = ShopifyAPI::Shop.current
-    
-  end
-  def form_page
-    if request.post?
-      if params[:name].present?
-        flash[:notice] = "Created #{ params[:colour] } unicorn: #{ params[:name] }."
-      else
-        flash[:error] = "Name must be set."
-      end
-    end
-  end
+    @webhooks = ShopifyAPI::Webhook.find(:all)    
+    @orders = ShopifyAPI::Order.find(:all) 
+    @shop = ShopifyAPI::Shop.current    
+  end 
 end
