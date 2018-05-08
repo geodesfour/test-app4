6bd614f3d804072aae93943bc1a7af44687a3ecc
Geoffrey Desfour <gdesfour@gmail.com>
2018-05-08 23:47:27 +0200

    add faq

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all)
     @webhooks = ShopifyAPI::Webhook.find(:all) 
-    @orders2 = ShopifyAPI::Order.find(:all)      
+    @orders2 = ShopifyAPI::Order.find(:all, :params => {:limit => 5, :order => "created_at DESC" })      
     @shop = ShopifyAPI::Shop.current    
   end 
 end
