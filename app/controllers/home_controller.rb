e8cdcce35b14866452ae9bf374d8e084cb381af6
Geoffrey Desfour <gdesfour@gmail.com>
2018-05-28 18:32:41 +0200

    hacks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all)
     @webhooks = ShopifyAPI::Webhook.find(:all)      
-    @shop = ShopifyAPI::Shop.current       
+    @shop = ShopifyAPI::Shop.current 
+    @asset = ShopifyAPI::Asset.find('layout/theme.liquid', :params => {:theme_id => 32311083050})      
   end   
 end
