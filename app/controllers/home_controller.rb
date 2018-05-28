ac155a7e83bc60ae196c18e6dfa50f9282a2b0e0
Geoffrey Desfour <gdesfour@gmail.com>
2018-05-28 20:51:06 +0200

    hacks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ShopifyApp::AuthenticatedController
     @webhooks = ShopifyAPI::Webhook.find(:all)      
     @shop = ShopifyAPI::Shop.current 
     @asset = ShopifyAPI::Asset.find('layout/theme.liquid')   
-    ShopifyAPI::Asset.new(:key => 'assets/test-special.css')   
+    @addasset = ShopifyAPI::Asset.new(:key => 'assets/test-special.css')   
   end   
 end
