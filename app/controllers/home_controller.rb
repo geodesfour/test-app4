e87acc0714f6d83601353f4ae1491b5d5c0dfb30
Geoffrey Desfour <gdesfour@gmail.com>
2018-05-28 20:41:32 +0200

    hacks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,16 +3,6 @@ class HomeController < ShopifyApp::AuthenticatedController
     @products = ShopifyAPI::Product.find(:all)
     @webhooks = ShopifyAPI::Webhook.find(:all)      
     @shop = ShopifyAPI::Shop.current 
-    @asset = ShopifyAPI::Asset.find('layout/theme.liquid')   
-    asset = ShopifyAPI::Asset.find('layout/theme.liquid')
-  # Grab a cache of the pre-installed layout
-  self.theme_layout = asset.value
-  snippet = DripSnippet.new(self.drip_account)
-  if asset.value.match(/#{snippet.start_wrapper}/i)
-    asset.value.sub!(/#{snippet.start_wrapper}.*#{snippet.end_wrapper}/im, snippet.snippet)
-  else
-    asset.value.sub!(/<\/body>/, "#{snippet.snippet}</body>")
-  end
-  return asset.save 
+    @asset = ShopifyAPI::Asset.find('layout/theme.liquid')      
   end   
 end
