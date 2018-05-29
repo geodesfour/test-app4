019f71c7233ac5b540c7136a900c3515f126176f
Geoffrey Desfour <gdesfour@gmail.com>
2018-05-29 11:05:43 +0200

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,5 @@ class HomeController < ShopifyApp::AuthenticatedController
     @products = ShopifyAPI::Product.find(:all)
     @webhooks = ShopifyAPI::Webhook.find(:all)      
     @shop = ShopifyAPI::Shop.current    
-    @asset = ShopifyAPI::Asset.new(:key => 'assets/test-special.css', :theme_id => 32311083050)   
   end   
 end
