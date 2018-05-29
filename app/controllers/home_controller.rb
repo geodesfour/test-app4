eee2cf62dcc0613ee80278f98f1b92920249c79b
Geoffrey Desfour <gdesfour@gmail.com>
2018-05-29 10:50:10 +0200

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all)
     @webhooks = ShopifyAPI::Webhook.find(:all)      
-    @shop = ShopifyAPI::Shop.current 
-    @asset = ShopifyAPI::Asset.find('layout/theme.liquid')   
-       
+    @shop = ShopifyAPI::Shop.current    
+    @asset = ShopifyAPI::Asset.new(:key => 'assets/test-special.css', :theme_id => 32311083050)   
   end   
 end
