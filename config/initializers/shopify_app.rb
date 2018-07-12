9de0e2a72b4f577409599494d2c1a5d743729cbc
Geoffrey Desfour <gdesfour@gmail.com>
2018-07-12 23:29:05 +0200

    toto

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -7,3 +7,22 @@ ShopifyApp.configure do |config|
   config.after_authenticate_job = false
   config.session_repository = Shop
 end
+
+
+def create_recurring_application_charge
+  unless ShopifyAPI::RecurringApplicationCharge.current
+    recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new(
+            name: "Speedfly Plan",
+            price: 3.99,
+            return_url: "https:\/\/#{APP_URL}\/activatecharge",
+            test: true,
+            trial_days: 7,
+            capped_amount: 100,
+            terms: "$0.99 for every order created")
+
+    if recurring_application_charge.save
+      @tokens[:confirmation_url] = recurring_application_charge.confirmation_url
+      redirect recurring_application_charge.confirmation_url
+    end
+  end
+end
