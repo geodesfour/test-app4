002c6b856247d24f04fd42ecf875d88cb134cb2a
Geoffrey Desfour <gdesfour@gmail.com>
2018-11-26 17:18:47 +0100

    ajout popup

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.application_name = "Speedfly - Boite à outils"
+  config.application_name = "Speedecom - APP"
   config.api_key = "5ec482eb67546ab3b00b5b624be3f1a7"
   config.secret = "f5abdbbb32aa7132153677838f8d7226"
   config.scope = "read_products, read_orders, read_themes, write_themes"
